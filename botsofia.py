ada9df5f01fb11d40437b3e2ed75adb21144ea23
noahso18fia-create <noahso18fia@gmail.com>
2026-08-16 21:46:36 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- botsofia.py
+++ botsofia.py
@@ -157,10 +157,6 @@ def home():
 def ping():
     return "OK", 200
 
-@app.route('/keep_alive')
-def keep_alive():
-    return "I am alive!", 200
-
 @app.route('/test/madrugada')
 def test_madrugada():
     enviar_saludo_madrugada()
